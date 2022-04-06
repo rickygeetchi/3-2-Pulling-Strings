332624e21b13a078bd91a291c349a05f5059bb5a
rickymsmith <35548840+rickymsmith@users.noreply.github.com>
2022-04-05 19:49:54 -0500

    My solutions to 3.2

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const first = "Roderick";
+
+
+console.log(first.toLowerCase());
+console.log(first.toUpperCase());
+console.log(first.replace("k","c"));
+console.log(first.replace("ck","x"));
+console.log(first + " Smith");
+console.log(`${first} Smith`);
